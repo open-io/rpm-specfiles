faee9fbbbef831c93b104d802a40c97a18715c73
Vincent Legoll <vincent.legoll@gmail.com>
2018-10-04 17:42:11 +0200

    Automate openio-grid version retrieval from spec file

--- openio-grid/get_private_asset.sh
+++ openio-grid/get_private_asset.sh
@@ -3,7 +3,7 @@
 TOKEN=""
 OWNER="open-io"
 REPO="oio-grid"
-TAG="0.4.0"
+TAG=$(awk '/^Version:/ {print $2 ; exit 0}' openio-grid.spec)
 
 RELEASES_URL="https://$TOKEN:@api.github.com/repos/$OWNER/$REPO/releases"
 ASSET_ID=$(curl $RELEASES_URL | jq 'map(select(.tag_name == "'"$TAG"'"))[0].assets[0].id')
